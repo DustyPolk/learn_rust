737b70eaf20bfdb26b11e14388cc3fd362116eb6
DustyPolk <Dpolk213@gmail.com>
2023-12-09 04:39:46 +0000

    Add multithreading to improve performance

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::thread;
+use std::time::Instant;
+
 fn main() {
-    println!("Hello, world!");
+    let now = Instant::now();
+
+    let handle = thread::spawn(|| {
+        let mut counter = 0;
+        while counter < 2_000_000_000 {
+            counter += 1;
+        }
+        counter
+    });
+
+    let result = handle.join().unwrap();
+    let elapsed = now.elapsed();
+
+    println!("Counted up to: {}", result);
+    println!("Time elapsed: {:?}", elapsed);
 }
